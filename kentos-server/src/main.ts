d669b3f2e10521de8e6bb4ba91e4dfb1ee6af581
Kurt Higa <kurthigaa@gmail.com>
2023-11-30 17:42:18 -0800

    set up form submission api

--- kentos-server/src/main.ts
+++ kentos-server/src/main.ts
@@ -1,4 +1,21 @@
-import { handler } from "./handler";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import { sendFormToAdmin } from "./services/sendFormToAdmin.ts";
+import type { FormData } from "./models/forms.ts";
 
-const port = Number(Deno.env.get("PORT") || 4202);
-Deno.serve({ port }, handler);
+const router = new Router();
+
+router.get("/forms/contact", async (context) => {
+  const body = await context.request.body();
+  const formData: FormData = body.value;
+
+  const response = sendFormToAdmin(formData);
+  context.response.body = response;
+});
+
+const app = new Application();
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
---
+++ kentos-server/src/services/sendFormToAdmin.ts
@@ -0,0 +1,4 @@
+import type { FormData } from "../models/forms.ts";
+export const sendFormToAdmin = ({ name, email, message }: FormData) => {
+  return name;
+};
