4589dd926910c7705fc7c28d5a98661392c52ab8
Kurt Higa <kurthigaa@gmail.com>
2023-11-30 18:27:30 -0800

    deno setup

--- kentos-server/src/main.ts
+++ kentos-server/src/main.ts
@@ -2,17 +2,18 @@ import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { sendFormToAdmin } from "./services/sendFormToAdmin.ts";
 import type { FormData } from "./models/forms.ts";
+// import { type RouterContext } from "https://deno.land/x/oak@v12.6.1/router.ts";
 
 const router = new Router();
 
-router.post("/forms/contact", async (context) => {
-  const body = await context.request.body();
-  console.log("Received Form Data:", JSON.stringify(body.value));
+router.post("/forms/contact", async (ctx) => {
+  const body = ctx.request.body();
+  console.log("Received Form Data:", JSON.stringify(await body.value));
 
-  const formData: FormData = body.value;
+  const formData: FormData = await body.value;
 
   const response = sendFormToAdmin(formData);
-  context.response.body = response;
+  ctx.response.body = response;
 });
 
 const app = new Application();
