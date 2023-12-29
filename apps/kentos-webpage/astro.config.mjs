e741983c7d29f346a76cfff9e48a6ad604231cfd
Kurt Higa <kurthigaa@gmail.com>
2023-12-29 13:14:44 -0600

    robot.txt support

--- apps/kentos-webpage/astro.config.mjs
+++ apps/kentos-webpage/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kentos.netlify.app',
   base: '/',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), robotsTxt()]
 });
