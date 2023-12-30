92259a66f6cabafbfe984447207e7414f865ee0b
Kurt Higa <kurthigaa@gmail.com>
2023-12-29 22:26:48 -0600

    sitemap fix

--- apps/kentos-webpage/astro.config.mjs
+++ apps/kentos-webpage/astro.config.mjs
@@ -2,11 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://talkstoryenglish.com',
   base: '/',
-  integrations: [tailwind(), react(), robotsTxt(), sitemap()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          ja: 'ja-JP',
+        },
+      },
+    }),
+  ],
 });
