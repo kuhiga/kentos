0fb0efcb07a2bdfad1dc4b23dd7c14b49f1ade85
Kurt Higa <kurthigaa@gmail.com>
2024-01-04 17:37:31 -0600

    update autocomplete to acccount fo rname

--- apps/kentos-webpage/src/components/Contact.tsx
+++ apps/kentos-webpage/src/components/Contact.tsx
@@ -148,7 +148,7 @@ const Contact = ({ lang }: { lang: string }) => {
                   onChange={handleInputChange}
                   name="firstName123lmp"
                   id="firstName123lmp"
-                  autoComplete="given-name"
+                  autoComplete={lang === "ja" ? "family-name" : "given-name"}
                   className="appearance-none block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   required
                 />
@@ -168,7 +168,7 @@ const Contact = ({ lang }: { lang: string }) => {
                   onChange={handleInputChange}
                   name="lastName123lmp"
                   id="lastName123lmp"
-                  autoComplete="family-name"
+                  autoComplete={lang === "ja" ? "given-name" : "family-name"}
                   className="appearance-none block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   required
                 />
